69fb8af7eadcd8ad8f14b536b463e2c067ab24d9
Finistelle <s.touatimail@gmail.com>
2017-09-03 19:12:44 +0200

    update gitignore

--- config/keys.js
+++ config/keys.js
@@ -1,3 +1,5 @@
-/**
- * Created by finistelle on 03/09/17.
- */
+module.exports = {
+  googleClientID:
+    '9852841106-u2k0obbbqoij4knvb4v8dvkq1enm4ell.apps.googleusercontent.com',
+  googleClientSecret: 'rsT_dihAyeHnSvecP0x8KN4d'
+};
