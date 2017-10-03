6767a821aa6f0b7828eccb13d1013e6b1248275a
Finistelle <s.touatimail@gmail.com>
2017-10-03 08:29:31 +0200

    setup passport

--- app.js
+++ app.js
@@ -3,11 +3,10 @@ const path = require('path');
 const favicon = require('serve-favicon');
 const logger = require('morgan');
 const cookieParser = require('cookie-parser');
+const cookieSession = require('cookie-session');
+const passport = require('passport');
 const bodyParser = require('body-parser');
-
-// services
-require('./server/models/user');
-require('./services/passport');
+const keys = require('./config/keys');
 
 // routes
 const index = require('./server/routes/index');
@@ -16,6 +15,15 @@ const oauth = require('./server/routes/oauth');
 
 const app = express();
 
+app.use(
+  cookieSession({
+    maxAge: 30 * 24 * 60 * 1000,
+    keys: [keys.cookieKey]
+  })
+);
+
+app.use(passport.initialize());
+app.use(passport.session());
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
@@ -30,7 +38,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', index);
 app.use('/auth/google', oauth);
-// app.use('/users', users);
+app.use('/users', users);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
--- services/passport.js
+++ services/passport.js
@@ -4,16 +4,26 @@ const keys = require('../config/keys');
 
 const User = require('../server/models/user');
 
+passport.serializeUser((user, done) => {
+  done(null, user.id);
+});
+
+passport.deserializeUser((id, done) => {
+  User.findById(id).then(user => {
+    done(null, user);
+  });
+});
+
 passport.use(
-    new GoogleStrategy(
-        {
-            clientID: keys.googleClientID,
-            clientSecret: keys.googleClientSecret,
-            callbackURL: '/auth/google/callback'
-        },
-        (accessToken, refreshToken, profile, done) => {
-            new User({googleId: profile.id});
-            console.log(User);
-        }
-    )
+  new GoogleStrategy(
+    {
+      clientID: keys.googleClientID,
+      clientSecret: keys.googleClientSecret,
+      callbackURL: '/auth/google/callback'
+    },
+    (accessToken, refreshToken, profile, done) => {
+      console.log(done);
+      new User({ googleId: profile.id });
+    }
+  )
 );
