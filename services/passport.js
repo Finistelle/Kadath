75ad39af3eeaac5182053d9996deef7a4867f222
Finistelle <s.touatimail@gmail.com>
2017-10-07 23:37:12 +0200

    fix google strategy proxy

--- services/passport.js
+++ services/passport.js
@@ -19,7 +19,8 @@ module.exports = function(passport, user) {
       {
         clientID: keys.googleClientID,
         clientSecret: keys.googleClientSecret,
-        callbackURL: '/auth/google/callback'
+        callbackURL: '/auth/google/callback',
+        proxy: true
       },
       (accessToken, refreshToken, profile, done) => {
         User.findOrCreate({
