cb349dbfd202708810cdb8ec12190475948ca6e2
Finistelle <s.touatimail@gmail.com>
2017-10-07 22:08:55 +0200

    update gitignore

---
+++ config/dev.js
@@ -0,0 +1,6 @@
+module.exports = {
+  googleClientID:
+    '9852841106-u2k0obbbqoij4knvb4v8dvkq1enm4ell.apps.googleusercontent.com',
+  googleClientSecret: 'rsT_dihAyeHnSvecP0x8KN4d',
+  cookieKey: 'fzofhzijzofbzjfbnzkjefspieijdqzam'
+};
